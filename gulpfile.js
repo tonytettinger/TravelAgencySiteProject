c3269485f632f1e68c0777cfec6d20ace15cdaac
Antal Tony Tettinger <antal.tettinger@gmail.com>
2017-12-27 13:26:41 +0800

    ok

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function(){
+    console.log("Hooray- you created a Gulp task");
+});
+
+gulp.task('html', function(){
+    console.log('whatsoever');
+});
+
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'))
+});
+
+gulp.task('watch', function(){
+    
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
+    });
+});
