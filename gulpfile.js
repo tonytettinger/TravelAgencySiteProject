327a1467b5f0d7c06181b29f81f09b16d0e4168b
Antal Tony Tettinger <antal.tettinger@gmail.com>
2017-12-27 21:01:32 +0800

    PostCSS Gulp use
    Following PostCSS lecture

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require ('autoprefixer');
 
 gulp.task('default', function(){
     console.log("Hooray- you created a Gulp task");
@@ -10,7 +12,10 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-    return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'))
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+    
 });
 
 gulp.task('watch', function(){
