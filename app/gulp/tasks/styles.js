19a2c415c017ec8fbe7c51483c4cbde2cfa31bf7
Antal Tony Tettinger <antal.tettinger@gmail.com>
2018-01-12 21:37:04 +0800

    Added responsive image practice
    Files added for responsive image practice

--- app/gulp/tasks/styles.js
+++ app/gulp/tasks/styles.js
@@ -3,12 +3,15 @@ autoprefixer = require ('autoprefixer'),
 nested= require('postcss-nested'),
 cssvars= require('postcss-simple-vars'),
 cssImport = require('postcss-import'),
-postcss = require('gulp-postcss');
-
+postcss = require('gulp-postcss'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested,autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested,autoprefixer]))
+        .on('error', function(errorInfo) {
+        console.log(errorInfo.toString());
+        this.emit('end');
+        })
         .pipe(gulp.dest('./app/temp/styles'));
-
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,2 @@
-require('./gulp/tasks/styles');
-require('.gulp/tasks/watch');
+require('./app/gulp/tasks/styles');
+require('./app/gulp/tasks/watch');
